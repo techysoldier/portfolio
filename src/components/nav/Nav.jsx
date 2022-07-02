0c20ed3a6cd3ccd00f80d6da772c639304b87637
devin <devinsymone@gmail.com>
2022-07-02 19:39:02 -0400

    floating nav component

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,17 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Nav.css'
 import {AiOutlineHome} from 'react-icons/ai'
 import{MdPersonOutline} from 'react-icons/md'
 import{BsBook} from 'react-icons/bs'
 import{BiMessage} from 'react-icons/bi'
 
+
 const Nav = () => {
+  const[ activeNav, setActiveNav] = useState('#')
   return (
     <nav>
-      <a href="#home"><AiOutlineHome/></a>
-      <a href="#about"><MdPersonOutline/></a>
-      <a href="#experience"><BsBook/></a>
-      <a href="#contact"><BiMessage/></a>
+      <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active': ''}><AiOutlineHome/></a>
+      <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav
+      === '#about' ? 'active': ''}><MdPersonOutline/></a>
+      <a href="#experience"onClick={() => setActiveNav('#experience')} className={activeNav
+      === '#experience' ? 'active': ''}><BsBook/></a>
+      <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav
+      === '#contact' ? 'active': ''}><BiMessage/></a>
     </nav>
   )
 }
