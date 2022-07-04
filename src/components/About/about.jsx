9f8652bae70b4e159be5f8cc4dce7f0a92a51b65
devin <devinsymone@gmail.com>
2022-07-04 16:03:03 -0400

    About page code adjustment and styling

--- src/components/About/about.jsx
+++ src/components/About/about.jsx
@@ -3,6 +3,7 @@ import './About.css'
 import anotherme from '../../images/anotherme.jpg'
 import {FaAward} from 'react-icons/fa'
 import {BiLibrary} from 'react-icons/bi'
+import {IoSchoolOutline} from 'react-icons/io5'
 
 const About = () => {
   return (
@@ -18,25 +19,31 @@ const About = () => {
         </div>
 
         <div className='about__content'>
-          <div className='about_cards'>
-            <article className='about_card'>
+          <div className='about__cards'>
+            <article className='about__card'>
               <FaAward className='about__icon'/>
               <h5>Experience</h5>
               <small>1+ Years Project Based Experience</small>
             </article>
 
-            <article className='about_card'>
+            <article className='about__card'>
               <BiLibrary className='about__icon'/>
               <h5>Projects</h5>
               <small>15+ Projects Completed</small>
+            </article>
+
+            <article className='about__card'>
+              <IoSchoolOutline className='about__icon'/>
+              <h5>Training</h5>
+              <small>DevCodeCamp Full Stack Development Course</small>
             </article>
           </div>
 
 
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto blanditiis adipisci, mollitia, 
-            earum quas totam optio tempora quaerat, quibusdam eligendi eaque fugiat ipsam sapiente expedita 
-            voluptatibus error. Ipsum, facere commodi?
+            Devin Symone Johnson completed a full stack software development course with DevCode Camp in 2022. 
+            She comes from a military and first responder background. She has always had an interest in technology,
+            and decided to try her hand at coding. After self teaching for a few months she decided to enroll in a program. 
           </p>
 
         
